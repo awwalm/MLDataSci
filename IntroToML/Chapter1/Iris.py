6aa83ea10a7cb705fe167a98aa411d5c1aa804cd
Awwal M <67423428+awwalm@users.noreply.github.com>
2022-09-25 20:59:17 +0400

    Added pair plots visualization

--- IntroToML/Chapter1/Iris.py
+++ IntroToML/Chapter1/Iris.py
@@ -2,6 +2,7 @@
 
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 
 # Iris dataset.
 # load_iris() returns a `Bunch` object, which is similar to a dictionary
@@ -33,4 +34,28 @@ print(iris['data'][:5])
 print(iris['target'])
 
 # Split dataset into training and test set
+# random_state parameter value 0f 0 allows the shuffling of dataset to be the same
 X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
+
+# Training (75%) and test set (25%)
+print(X_train.shape)
+print(X_test.shape)
+
+# Preliminary visualization - pair plot which takes all pairs of two features
+fig, ax = plt.subplots(3, 3, figsize=(15, 15))
+plt.suptitle("iris_pairplot")
+
+for i in range(3):
+    for j in range(3):
+        ax[i, j].scatter(X_train[:, j], X_train[:, i+1], c=y_train, s=60)
+        ax[i, j].set_xticks(())
+        ax[i, j].set_yticks(())
+        if i == 2:
+            ax[i, j].set_xlabel(iris['feature_names'][j])
+        if j == 0:
+            ax[i, j].set_ylabel(iris['feature_names'][i+1])
+        if j > i:
+            ax[i, j].set_visible(False)
+plt.show()
+
+# input("press any key to close")
